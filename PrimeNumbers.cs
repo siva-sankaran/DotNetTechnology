f0e0962adb48b53a1467b82c76c44d2a00e16609
Siva Sankaran S <sivasankaran.somas-kandan@adp.com>
2015-12-14 12:54:59 +0530

    Learn to use Ienumerable interface

---
+++ PrimeNumbers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+public class PrimeNumberEnum : IEnumerator
+{
+    ulong _currentPrime;
+    ulong _capacity;
+    public PrimeNumberEnum(ulong Capacity)
+    {
+        _currentPrime = 1;
+        _capacity = Capacity;
+    }
+
+    public bool MoveNext()
+    {
+        ulong next = _currentPrime, upperLimit, counter;
+        do
+        {
+            next = next + 1;
+            upperLimit = (ulong)next / 2;
+
+            counter = 2;
+            while (counter <= upperLimit)
+            {
+                if (next % counter == 0)
+                    break;
+                counter++;
+            }
+        } while (counter <= upperLimit);
+
+        if (next <= _capacity)
+        {
+            _currentPrime = next;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Reset()
+    {
+        _currentPrime = 1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public ulong Current
+    {
+        get
+        {
+            return _currentPrime;
+        }
+    }
+}
+
+public class PrimeNumbers : IEnumerable
+{
+    ulong _capacity;
+    public PrimeNumbers(ulong Capacity)
+    {
+        _capacity = Capacity;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (IEnumerator)GetEnumerator();
+    }
+
+    public PrimeNumberEnum GetEnumerator()
+    {
+        return new PrimeNumberEnum(_capacity);
+    }
+}
